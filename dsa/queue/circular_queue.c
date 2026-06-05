cd43b16654c64fec5adae76092a2ef18c559a6fa
Arjun Vasavan <vasavanarjun@gmail.com>
2026-06-05 20:32:33 +0530

    🔰

--- dsa/queue/circular_queue.c
+++ dsa/queue/circular_queue.c
@@ -5,34 +5,34 @@ int queue[SIZE];
 int front = -1, rear = -1;
 
 int isFull() {
-    return (front == (rear + 1) % SIZE);
+    return (front == (rear + 1) % SIZE); /* here we are rounding and checking is anything left before front */
 }
 
 int isEmpty() {
-    return (front == -1);
+    return (front == -1); /* this means its an -1 flag telling we havent even added anything */
 }
 
 void enqueue(int val) {
-    if (isFull()) {
+    if (isFull()) { /* before adding checking if queue is full or not */
         printf("Queue Full\n");
         return;
     }
-    if (isEmpty()) front = 0;
-    rear = (rear + 1) % SIZE;
-    queue[rear] = val;
+    if (isEmpty()) front = 0; /* if empty we are making front as 0 */
+    rear = (rear + 1) % SIZE; /* this will round out and find the left spot we can add */
+    queue[rear] = val; /* adding value to it */
     printf("Enqueued: %d\n", val);
 }
 
 int dequeue() {
-    if (isEmpty()) {
+    if (isEmpty()) { /* we cannot dequeue if queue is empty */
         printf("Queue Empty\n");
         return -1;
     }
-    int val = queue[front];
-    if (front == rear) {
+    int val = queue[front]; /* taking out the value from front */
+    if (front == rear) { /* this is to check if last element is also removed */
         front = rear = -1;  // last element removed
     } else {
-        front = (front + 1) % SIZE;
+        front = (front + 1) % SIZE; /* else we are adding element */
     }
     return val;
 }
